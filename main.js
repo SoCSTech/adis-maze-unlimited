d0b1a47472f1d6bafc5e0ca5feea69d254ef4f04
Josh Cooper <joshcooper@lincoln.ac.uk>
2024-12-16 12:30:06 +0000

    feat: initial commit
    wrap the game inside of an electron app for the basic first steps

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        fullscreen: true
+    })
+
+    win.loadFile('index.html')
+}   
